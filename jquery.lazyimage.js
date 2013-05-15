a8026a64bc624dbb6a34be8fe81261e9e908daec
Dzulqarnain Nasir <dzul@geta.no>
2013-05-15 11:35:55 +0300

    Initial commit for ImageResizer.NET

--- jquery.lazyimage.js
+++ jquery.lazyimage.js
@@ -1,5 +1,5 @@
 /* 
- *   jQuery LazyImage Plugin 1.0.1
+ *   jQuery LazyImage Plugin for ImageResizer.NET 1.0.0
  *   https://github.com/dnasir/jquery-lazyimage
  *
  *   Copyright 2013, Dzulqarnain Nasir
@@ -12,6 +12,7 @@
 (function($, window) {
     var win = $(window);
 
+    // Helpers
     function isInViewport(el) {
         var viewport = {
             top : win.scrollTop(),
@@ -33,58 +34,142 @@
             );
     }
 
+    function sortAscending(a, b) {
+        return a - b;
+    }
+
+    function replaceParam(url, param) {
+        var newParam = param.key + '=' + param.value;
+        return url.replace(new RegExp(param.key + '=', 'gi'), '$1', newParam);
+    }
+
+    function setGetParameter(url, paramName, paramValue) {
+        if (url.indexOf(paramName + "=") >= 0) {
+            var prefix = url.substring(0, url.indexOf(paramName));
+            var suffix = url.substring(url.indexOf(paramName)).substring(url.indexOf("=") + 1);
+            suffix = (suffix.indexOf("&") >= 0) ? suffix.substring(suffix.indexOf("&")) : "";
+            url = prefix + paramName + "=" + paramValue + suffix;
+        }
+        else {
+            if (url.indexOf("?") < 0)
+                url += "?" + paramName + "=" + paramValue;
+            else
+                url += "&" + paramName + "=" + paramValue;
+        }
+        return url;
+    }
+
+    // LazyImage constructor
     function LazyImage(img) {
+        // Public properties
         var self = this;
         self.img = $(img);
-        self.src = self.img.data('src');
-        self.useOriginalSize = self.img.data('use-original-size');
         self.loaded = false;
+
+        // Private properties
+        var src = self.img.data('src'),
+            useOriginalSize = self.img.data('use-original-size'),
+            responsive = eval(self.img.data('responsive')),
+            fadeIn = self.img.data('fade-in');
 
         self.img.css({
             width: self.img.attr('width'),
             height: self.img.attr('height')
         });
 
-        self.loadImage = function() {
-            $('<img />')
+        // Public methods
+        self.loadImage = function () {
+            var temp = $('<img />')
                 .on('load', function () {
                     self.img
                         .removeAttr('style')
                         .css('opacity', 0)
-                        .attr('src', self.src);
+                        .attr('src', src);
 
-                    if(self.useOriginalSize) {
+                    if(useOriginalSize) {
                         self.img.attr({
                             width: this.width,
                             height: this.height
                         });
                     }
 
-                    self.img.animate({ opacity: 1 }).addClass('lazy-loaded');
+                    if (fadeIn) {
+                        self.img.animate({ opacity: 1 });
+                    } else {
+                        self.img.css({ opacity: 1 })
+                    }
+
                     self.loaded = true;
-                    self.img.trigger('lazyLoaded');
+                    self.img.addClass('lazy-loaded').trigger('lazyLoaded');
                 })
-                .attr('src', self.src);
+                .attr('src', src);
+
+            if (responsive) {
+                var currentWidth;
+                var breakpoints = $.map(responsive, function (val) {
+                    return val.step;
+                });
+
+                if(!breakpoints.length) {
+                    return;
+                }
+
+                breakpoints.sort(sortAscending);
+
+                var t;
+                function resize() {
+                    clearTimeout(t);
+                    t = setTimeout(function () {
+                        var width = win.width(),
+                            _breakpoints = new Array();
+
+                        if (win[0].scrollHeight > win.innerHeight()) {
+                            width += 30;
+                        }
+
+                        _breakpoints = _breakpoints.concat(breakpoints);
+                        _breakpoints.push(width);
+                        _breakpoints.sort(sortAscending); // figure out where we fit in
+
+                        var index = _breakpoints.indexOf(width) - 1;
+                        index = index <= 0 ? 0 : (index >= (_breakpoints.length - 1) ? (breakpoints.length - 1) : index);
+
+                        var step = $.grep(responsive, function (s) { return s.step === breakpoints[index]; })[0];
+                        if (step && step.width != currentWidth) {
+                            temp.attr('src', setGetParameter(src, 'width', step.width));
+                            currentWidth = step.width;
+                        }
+                    }, 300);
+                }
+
+                win.on('resize', resize);
+                self.img.one('lazyLoaded', resize);
+            }
         };
     }
 
-    $.fn.lazyImage = function() {
+    // jQuery interface
+    $.fn.lazyImage = function () {
+        // Build array of LazyImage objects
         var images = this.map(function() {
             return new LazyImage(this);
         });
 
         function update() {
-            $.each(images, function() {
+            $.each(images, function () {
+                // Load images as it appears in the viewport
                 if (isInViewport(this.img) && !this.loaded) {
                     this.loadImage();
                 }
             });
 
+            // Once all images have been processed, remove event handler
             if (!images.length) {
                 win.off('load scroll resize', update);
             }
         }
 
+        // Remove images that have been loaded from array
         this.each(function() {
             $(this).on('lazyLoaded', function() {
                 images = $.grep(images, function(img) {
@@ -93,8 +178,10 @@
             });
         });
 
+        // Set up event handler
         win.on('load scroll resize', update);
     };
 
+    // Auto attach to matching elements
     $('img.lazy-image[data-src]').lazyImage();
 })(jQuery, window);
